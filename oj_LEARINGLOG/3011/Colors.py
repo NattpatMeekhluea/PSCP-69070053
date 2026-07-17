8dbe7ebc13cac985c874b068d726b70f89e529b9
NattpatMeekhluea <69070053@kmitl.ac.th>
2026-07-17 15:57:04 +0700

    LEARINGLOG_COLORS

--- oj_LEARINGLOG/3011/Colors.py
+++ oj_LEARINGLOG/3011/Colors.py
@@ -1,1 +1,35 @@
 """Colors"""
+
+def main():
+    """Colors"""
+    firstcolors = input()
+    secondcolors = input()
+
+    if (firstcolors == "Red" and secondcolors == "Yellow"
+    or firstcolors == "Yellow" and secondcolors == "Red"
+    ):
+        print("Orange")
+    elif (firstcolors == "Red" and secondcolors == "Red"
+    or firstcolors == "Red" and secondcolors == "Red"
+    ):
+        print("Red")
+    elif (firstcolors == "Red" and secondcolors == "Blue"
+    or firstcolors == "Blue" and secondcolors == "Red"
+    ):
+        print("Violet")
+    elif (firstcolors == "Yellow" and secondcolors == "Yellow"
+    or firstcolors == "Yellow" and secondcolors == "Yellow"
+    ):
+        print("Yellow")
+    elif (firstcolors == "Yellow" and secondcolors == "Blue"
+    or firstcolors == "Blue" and secondcolors == "Yellow"
+    ):
+        print("Green")
+    elif (firstcolors == "Blue" and secondcolors == "Blue"
+    or firstcolors == "Blue" and secondcolors == "Blue"
+    ):
+        print("Blue")
+    else:
+        print("Error")
+
+main()
