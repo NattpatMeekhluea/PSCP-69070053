031af4e70dbc0aadd2f6c240d590af0d9fbf563d
ColaColaPepsiInwza007 <69070053@kmitl.ac.th>
2026-07-19 21:14:01 +0700

    LEARINGLOG_COLORS_SUBMISSION

--- PROBLEMCODING/ฝึกฝน.py
+++ PROBLEMCODING/ฝึกฝน.py
@@ -1,18 +1,1 @@
-money = int(input())
-
-banknote_1000 = money // 1000    
-remaining_money = money % 1000
-
-
-banknote_100 = remaining_money // 100   
-remaining_money = remaining_money % 100
-
-
-coin10 = remaining_money // 10
-remaining_money = remaining_money % 10
-
-coin1 = remaining_money
-
-print(banknote_1000)
-print(banknote_100)
-print(coin10)
+""
